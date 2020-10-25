36ffc2b9df31079cb8c4d885972e587091158544
ForteEscape <sehun5515@naver.com>
2020-10-25 19:26:15 +0900

    add week1 sample code

---
+++ Chap1_basic/basic2.py
@@ -0,0 +1,16 @@
+str1 = "hello world"
+
+print(str1[-1])
+print(str1[:2])
+print(str1[:2] + str1[2:])
+
+listNum = [1, 2, 3, 4, 5]
+
+print(listNum[3])
+print(listNum)
+
+listNum[3] = 5
+print(listNum)
+
+listNum.append(6)
+print(listNum)
